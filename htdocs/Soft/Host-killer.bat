178cd3f32ae11da12324276913bd51e4ae4d0dac
Vladimir <adeptius@gmail.com>
2016-08-07 20:05:47 +0300

    Added

---
+++ htdocs/Soft/Host-killer.bat
@@ -0,0 +1,4 @@
+erase %SystemRoot%\System32\drivers\etc\hosts 
+erase %SystemRoot%\System32\drivers\etc\hosts  /f
+erase %SystemRoot%\System32\drivers\etc\hosts  /a:h
+erase %SystemRoot%\System32\drivers\etc\hosts /f /a:h
